2f06b6bef13a63c3e9aff6169860c146f88e5d0b
Magda <100711006+amelangad@users.noreply.github.com>
2023-09-30 14:37:56 +0200

    Delete src directory

--- src/images.js
+++
@@ -1,8 +0,0 @@
-import image1 from '../src/assets/kamar.jpg';
-import image2 from '../src/assets/beauty.jpg';
-import image3 from '../src/assets/bigbag.png';
-import image4 from '../src/assets/shotx.png';
-import image5 from '../src/assets/movies.png';
-import image6 from '../src/assets/3d.png';
-
-export default [image1, image2, image3, image4, image5, image6];
